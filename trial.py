2f378fd6f8d36eb8eedeee1970a34d2ac2e1ef94
Vachana Shetty <vachanagshetty@gmail.com>
2020-10-02 14:34:44 -0700

    Mfs_pacman

---
+++ trial.py
@@ -0,0 +1,16 @@
+# tuple_list = ([1,2,3], ['label1', 'label2', 'label3'])
+# for val, label in zip(*tuple_list):
+#     print(label)
+
+# a = [((34, 15), 'South', 1), ((33, 16), 'West', 1)]
+# # for val, label in zip(*a):
+# #     print(val)
+# print(len(a))
+# for i in range(len(a)):
+#     print(a[i][0][1])
+#     print(a[i])
+
+VisitedCells = []
+VisitedCells.append
+
+
